58b94c0730d58f062e021030481408a3417c06ce
MyoungJinSeo <sih5673@gmail.com>
2023-12-05 00:28:31 +0900

    Start Setting

---
+++ src/main/java/com/plate/silverplate/SilverPlateApplication.java
@@ -0,0 +1,13 @@
+package com.plate.silverplate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SilverPlateApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SilverPlateApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/plate/silverplate/SilverPlateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.plate.silverplate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SilverPlateApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
