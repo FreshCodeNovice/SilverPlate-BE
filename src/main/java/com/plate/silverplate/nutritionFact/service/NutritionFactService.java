c8550b61f75e5d3c0702afd25bf5dd27eca48c2d
MyoungJinSeo <sih5673@gmail.com>
2024-01-08 23:47:08 +0900

    feat : countBy #17

--- src/main/java/com/plate/silverplate/nutritionFact/service/NutritionFactService.java
+++ src/main/java/com/plate/silverplate/nutritionFact/service/NutritionFactService.java
@@ -8,6 +8,8 @@ import com.plate.silverplate.common.exception.ErrorException;
 import com.plate.silverplate.nutritionFact.domain.entity.NutritionFact;
 import com.plate.silverplate.nutritionFact.domain.repo.NutritionFactRepository;
 import com.plate.silverplate.nutritionFact.dto.response.NutritionFactResponse;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
 import jakarta.annotation.PostConstruct;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
@@ -15,11 +17,14 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.client.RestClient;
 import com.fasterxml.jackson.core.type.TypeReference;
+import org.springframework.web.client.RestTemplate;
+
 import java.util.List;
 
 @Service
 @Transactional(readOnly = true)
 @RequiredArgsConstructor
+@EnableScheduling
 public class NutritionFactService {
 
     @Value("${nutrition.key_id}")
@@ -29,10 +34,13 @@ public class NutritionFactService {
     private String serviceId;
     private final NutritionFactRepository nutritionFactRepository;
 
+    private final RestTemplate restTemplate;
     // 우선 스케줄러와 데이터를 박아놓는 거 중에서 고민중
-    @PostConstruct
+    @Scheduled(cron = "30 50 17 * * *")
+//    @PostConstruct
     public void listCreate(){
-        for (int i = 0; i < 92; i++) {
+        Long countAll = nutritionFactRepository.countBy();
+        for (int i = (int) (countAll/1000); i < 92; i++) {
             int startIdx = 1 + 1000*i;
             int endIdx = (i+1) *1000;
             createNutrition(startIdx,endIdx);
@@ -42,10 +50,11 @@ public class NutritionFactService {
     }
 
     /*
-    * url에 있는 값 String 형태로 받아오는 메소드
-    * */
+     * url에 있는 값 String 형태로 받아오는 메소드
+     * */
     private String getNutrition(int startIdx, int endIdx){
-        RestClient restClient = RestClient.create();
+
+        RestClient restClient = RestClient.create(restTemplate);
 
         return  restClient.get()
                 .uri("http://openapi.foodsafetykorea.go.kr/api/{keyId}/{serviceId}/json/{startIdx}/{endIdx}" ,keyId , serviceId,startIdx,endIdx)
@@ -53,12 +62,14 @@ public class NutritionFactService {
                 .body(String.class);
 
     }
-/*
-* getNutrition 메소드를 통해 jsonString 값 받아오기
-* getListNutritionDto를 통해  List<NutritionFactResponse> 형변환
-* toListEntity를 통해 dto 리스트값들 entity값으로 변환
-* 변환된 엔티티를 저장하는 메소드
-* */
+
+
+    /*
+     * getNutrition 메소드를 통해 jsonString 값 받아오기
+     * getListNutritionDto를 통해  List<NutritionFactResponse> 형변환
+     * toListEntity를 통해 dto 리스트값들 entity값으로 변환
+     * 변환된 엔티티를 저장하는 메소드
+     * */
     @Transactional
     public void createNutrition(int startIdx, int endIdx){
         String response = getNutrition(startIdx, endIdx);
@@ -71,9 +82,9 @@ public class NutritionFactService {
     }
 
     /*
-    * jsonString 값을 list<NutritionFactResponse> 형 변환 메소드
-    * ObjectMapper를 통해 json 코드 안에 row 데이터에 필요한 정보 불러옴
-    * */
+     * jsonString 값을 list<NutritionFactResponse> 형 변환 메소드
+     * ObjectMapper를 통해 json 코드 안에 row 데이터에 필요한 정보 불러옴
+     * */
     public List<NutritionFactResponse> getListNutritionDto(String response) {
         ObjectMapper objectMapper = new ObjectMapper();
 
