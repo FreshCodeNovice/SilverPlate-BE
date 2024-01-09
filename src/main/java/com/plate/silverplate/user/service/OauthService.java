d71c0c0220922034d12089bdaebb39ede7793ab8
jiseon <mongbu54@gmail.com>
2024-01-09 20:28:38 +0900

    feat : OAuth2UserService 구현하는 것으로 변경

--- src/main/java/com/plate/silverplate/user/service/OauthService.java
+++ src/main/java/com/plate/silverplate/user/service/OauthService.java
@@ -1,127 +1,95 @@
 package com.plate.silverplate.user.service;
 
-import com.plate.silverplate.user.domain.dto.KakaoUserInfo;
-import com.plate.silverplate.user.domain.dto.LoginResponse;
-import com.plate.silverplate.user.domain.dto.Oauth2UserInfo;
-import com.plate.silverplate.user.domain.dto.OauthTokenResponse;
+import com.plate.silverplate.user.domain.dto.OAuth2Attribute;
 import com.plate.silverplate.user.domain.entity.User;
-import com.plate.silverplate.user.jwt.JwtTokenProvider;
+import com.plate.silverplate.user.domain.entity.UserProfile;
 import com.plate.silverplate.user.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.core.ParameterizedTypeReference;
-import org.springframework.http.MediaType;
-import org.springframework.security.oauth2.client.registration.ClientRegistration;
-import org.springframework.security.oauth2.client.registration.InMemoryClientRegistrationRepository;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.oauth2.client.userinfo.DefaultOAuth2UserService;
+import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
+import org.springframework.security.oauth2.client.userinfo.OAuth2UserService;
+import org.springframework.security.oauth2.core.OAuth2AuthenticationException;
+import org.springframework.security.oauth2.core.user.DefaultOAuth2User;
+import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.util.LinkedMultiValueMap;
-import org.springframework.util.MultiValueMap;
-import org.springframework.web.reactive.function.client.WebClient;
 
-import java.nio.charset.StandardCharsets;
 import java.util.Collections;
 import java.util.Map;
+import java.util.Optional;
 
 @Slf4j
 @RequiredArgsConstructor
 @Service
-public class OauthService {
-    private static String BEARER_TYPE = "Bearer";
-    private final InMemoryClientRegistrationRepository inMemoryRepository;
+public class OauthService implements OAuth2UserService<OAuth2UserRequest, OAuth2User> {
     private final UserRepository userRepository;
-    private final JwtTokenProvider jwtTokenProvider;
 
-    /**
-     * @InMemoryRepository application-oauth properties 정보를 담고 있음
-     * @getToken() 넘겨받은 code로 Oauth 서버에 Token 요청
-     * @getUserProfile 첫 로그인 시 회원가입
-     * 유저 인증 후 Jwt AccessToken, Refresh Token 생성
-     * TODO: REDIS에 Refresh Token 저장
-     */
+    @Override
+    public OAuth2User loadUser(OAuth2UserRequest userRequest) throws OAuth2AuthenticationException {
+        // 기본 OAuth2UserService 객체 생성
+        OAuth2UserService<OAuth2UserRequest, OAuth2User> oAuth2UserService = new DefaultOAuth2UserService();
+
+        // OAuth2UserService를 사용하여 OAuth2User 정보를 가져온다.
+        OAuth2User oAuth2User = oAuth2UserService.loadUser(userRequest);
+
+        // 클라이언트 등록 ID(google, naver, kakao)와 사용자 이름 속성을 가져온다.
+        String registrationId = userRequest.getClientRegistration().getRegistrationId();
+        String userNameAttributeName = userRequest.getClientRegistration()
+                .getProviderDetails().getUserInfoEndpoint().getUserNameAttributeName();
+
+        // OAuth2UserService를 사용하여 가져온 OAuth2User 정보로 OAuth2Attribute 객체를 만든다.
+        OAuth2Attribute oAuth2Attribute =
+                OAuth2Attribute.of(registrationId, userNameAttributeName, oAuth2User.getAttributes());
+
+        // OAuth2Attribute의 속성값들을 Map으로 반환 받는다.
+        Map<String, Object> memberAttribute = oAuth2Attribute.convertToMap();
+
+        // 사용자 email(또는 id) 정보를 가져온다.
+        String email = (String) memberAttribute.get("email");
+        // 이메일로 가입된 회원인지 조회한다.
+        Optional<User> findUser = userRepository.findByEmail(email);
+
+        log.error((String) memberAttribute.get("id"));
+
+        if (findUser.isEmpty()) {
+            // 회원이 존재하지 않는 경우
+            memberAttribute.put("exist", false);
+            return new DefaultOAuth2User(
+                    Collections.singleton(new SimpleGrantedAuthority("ROLE_USER")),
+                    memberAttribute, "email");
+        }
+
+        UserProfile oldProfile = findUser.get().getUserProfile();
+        UserProfile newProfile = UserProfile.builder()
+                .nickName(oAuth2Attribute.getName())
+                .provider(oAuth2Attribute.getProvider())
+                .imageUrl(oAuth2Attribute.getPicture())
+                .build();
+
+        // 프로필이 변경되었으면 갱신
+        if (!oldProfile.equals(newProfile)) {
+            findUser.get().updateUserProfile(newProfile);
+            userRepository.save(findUser.get());
+        }
+
+        // 회원이 존재할 경우
+        memberAttribute.put("exist", true);
+        return new DefaultOAuth2User(
+                Collections.singleton(new SimpleGrantedAuthority(findUser.get().getRole())),
+                memberAttribute, "email");
+    }
 
     @Transactional
-    public LoginResponse login(String providerName, String code) {
-        ClientRegistration provider = inMemoryRepository.findByRegistrationId(providerName);
-        OauthTokenResponse tokenResponse = getToken(code, provider);
-        User user = getUserProfile(providerName, tokenResponse, provider);
+    public void save(OAuth2User oAuth2User) {
+        String email = oAuth2User.getAttribute("email");
+        String provider = oAuth2User.getAttribute("provider");
+        String name = oAuth2User.getAttribute("name");
+        String imageUrl = oAuth2User.getAttribute("picture");
 
-        String accessToken = jwtTokenProvider.createAccessToken(String.valueOf(user.getId()));
-        String refreshToken = jwtTokenProvider.createRefreshToken();
-
-        return LoginResponse.builder()
-                .id(user.getId())
-                .nickName(user.getUserProfile().getNickName())
-                .email(user.getEmail())
-                .imageUrl(user.getUserProfile().getImageUrl())
-                .role(user.getRole())
-                .tokenType(BEARER_TYPE)
-                .accessToken(accessToken)
-                .refreshToken(refreshToken)
-                .build();
-    }
-
-    private OauthTokenResponse getToken(String code, ClientRegistration provider) {
-        return WebClient.create()
-                    .post()
-                    .uri(provider.getProviderDetails().getTokenUri())
-                    .headers(header -> {
-                        header.setContentType(MediaType.APPLICATION_FORM_URLENCODED);
-                        header.setAcceptCharset(Collections.singletonList(StandardCharsets.UTF_8));
-                    })
-                    .bodyValue(tokenRequest(code, provider))
-                    .retrieve()
-                    .bodyToMono(OauthTokenResponse.class)
-                    .block();
-    }
-
-    private MultiValueMap<String, String> tokenRequest(String code, ClientRegistration provider) {
-        MultiValueMap<String, String > formData = new LinkedMultiValueMap<>();
-
-        formData.add("code", code);
-        formData.add("grant_type", "authorization_code");
-        formData.add("redirect_uri", provider.getRedirectUri());
-        formData.add("client_secret", provider.getClientSecret());
-        formData.add("client_id", provider.getClientId());
-
-        return formData;
-    }
-
-    private User getUserProfile(String providerName, OauthTokenResponse tokenResponse, ClientRegistration provider) {
-
-        Map<String, Object> userAttributes = getUserAttributes(provider, tokenResponse);
-        Oauth2UserInfo oauth2UserInfo = null;
-
-        if (providerName.equals("kakao")) {
-            oauth2UserInfo = new KakaoUserInfo(userAttributes);
-        } else {
-            log.info("허용되지 않은 접근 입니다.");
-        }
-
-        String provide = oauth2UserInfo.getProvider();
-        String providerId = oauth2UserInfo.getProviderId();
-        String nickName = oauth2UserInfo.getNickName();
-        String email = oauth2UserInfo.getEmail();
-        String imageUrl = oauth2UserInfo.getImageUrl();
-
-        User userEntity = userRepository.findByEmail(email);
-
-        if (userEntity == null) {
-            userEntity = User.createUser(email, nickName, provide, providerId, imageUrl);
-            userRepository.save(userEntity);
-        }
-
-        return userEntity;
-    }
-
-    private Map<String, Object> getUserAttributes(ClientRegistration provider, OauthTokenResponse tokenResponse) {
-        return WebClient.create()
-                .get()
-                .uri(provider.getProviderDetails().getUserInfoEndpoint().getUri())
-                .headers(header -> header.setBearerAuth(tokenResponse.getAccess_token()))
-                .retrieve()
-                .bodyToMono(new ParameterizedTypeReference<Map<String, Object>>() {})
-                .block();
+        User user = User.createUser(email, name, provider, imageUrl);
+        userRepository.save(user);
     }
 }
 
