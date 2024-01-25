a29531f8e7d65260b6a353215e33037d5d2e5a5f
jiseon <mongbu54@gmail.com>
2024-01-25 10:17:30 +0900

    feat: 신체 정보 조회, 수정 api 추가

--- src/main/java/com/plate/silverplate/userPhysical/controller/UserPhysicalController.java
+++ src/main/java/com/plate/silverplate/userPhysical/controller/UserPhysicalController.java
@@ -1,16 +1,16 @@
 package com.plate.silverplate.userPhysical.controller;
 
 import com.plate.silverplate.user.domain.entity.User;
-import com.plate.silverplate.userPhysical.domain.dto.UserPhysicalRequest;
+import com.plate.silverplate.userPhysical.domain.dto.request.UserPhysicalRequest;
+import com.plate.silverplate.userPhysical.domain.dto.response.UserPhysicalCreateResponse;
+import com.plate.silverplate.userPhysical.domain.dto.response.UserPhysicalResponse;
+import com.plate.silverplate.userPhysical.domain.entity.UserPhysical;
 import com.plate.silverplate.userPhysical.service.UserPhysicalService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RequestMapping("/api/v1/userPhysicals")
 @RequiredArgsConstructor
@@ -19,9 +19,24 @@ public class UserPhysicalController {
     private final UserPhysicalService userPhysicalService;
 
     @PostMapping("")
-    public ResponseEntity<String> createUserPhysical(@AuthenticationPrincipal User user,
+    public ResponseEntity<UserPhysicalCreateResponse> createUserPhysical(@AuthenticationPrincipal User user,
                                                      @Valid @RequestBody UserPhysicalRequest physicalRequest) {
-        Long createdId = userPhysicalService.createUserPhysical(physicalRequest, user);
-        return ResponseEntity.ok("Created User Physical Id: " + createdId);
+        UserPhysical userPhysical = userPhysicalService.createUserPhysical(physicalRequest, user);
+        return ResponseEntity.ok(UserPhysicalCreateResponse.of(userPhysical));
+    }
+
+    @GetMapping("")
+    public ResponseEntity<UserPhysicalResponse> getUserPhysical(@AuthenticationPrincipal User user) {
+        UserPhysical userPhysical = userPhysicalService.getUserPhysical(user);
+
+        return ResponseEntity.ok(UserPhysicalResponse.of(userPhysical));
+    }
+
+    @PatchMapping("")
+    public ResponseEntity<UserPhysicalResponse> updateUserPhysical(@AuthenticationPrincipal User user,
+                                                                   @Valid @RequestBody UserPhysicalRequest physicalRequest) {
+        UserPhysical userPhysical = userPhysicalService.updateUserPhysical(physicalRequest, user);
+
+        return ResponseEntity.ok(UserPhysicalResponse.of(userPhysical));
     }
 }
--- src/main/java/com/plate/silverplate/userPhysical/repository/UserPhysicalRepository.java
+++ src/main/java/com/plate/silverplate/userPhysical/repository/UserPhysicalRepository.java
@@ -1,7 +1,11 @@
 package com.plate.silverplate.userPhysical.repository;
 
+import com.plate.silverplate.user.domain.entity.User;
 import com.plate.silverplate.userPhysical.domain.entity.UserPhysical;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UserPhysicalRepository extends JpaRepository<UserPhysical, Long> {
+    Optional<UserPhysical> findUserPhysicalByUser(User user);
 }
